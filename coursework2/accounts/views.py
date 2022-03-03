726a03963817f7eca2f1126a62dddf9d88ecff8d
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-03 20:11:01 +0800

    added to accounts views to go through form and added url directory

--- coursework2/accounts/views.py
+++ coursework2/accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+
+from coursework2.accounts.forms import RegistrationForm
+from .models import *
 
 # Create your views here.
+def register(request, *args, **kwargs):
+    user = request.user
+    if user.is_authenticated:
+        return HttpResponse("You are authenticated as {user.email}")
+    context = {}
+
+    if request.method == 'POST':
+        registration_form = RegistrationForm(data=request.POST)
+        if registration_form.is_valid():
+            #Creates account
+            registration_form.save()
+            #retrieve the needed things to automatically login for the user
+            email = registration_form.cleaned_data.get('email').lower()
+            password1 = registration_form.cleaned_data.get('password1')
+            account = authenticate(email=email,password=password1)
+            login(request, account)
+            # aftr logging in redirect to login
+            destination = kwargs.get("next")
+            if destination:
+                return redirect(destination)
+            return redirect("home")
+        else:
+            context['registration_form'] = registration_form
+
+    return render(request, 'accounts/register.html', context)

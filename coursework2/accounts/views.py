9876141eb38145bd8aded1c9b103b74eb6ffee81
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-04 10:54:23 +0800

    login view added

--- coursework2/accounts/views.py
+++ coursework2/accounts/views.py
@@ -1,3 +1,4 @@
+import email
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
@@ -7,6 +8,8 @@ from .models import *
 from django.contrib import messages
 
 # Create your views here.
+
+# Register View
 def register(request, *args, **kwargs):
     user = request.user
     if user.is_authenticated:
@@ -28,3 +31,28 @@ def register(request, *args, **kwargs):
         context['registration_form'] = registration_form
 
     return render(request, 'accounts/register.html', context)
+
+# Login View
+def login(request):
+    context={}
+
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            
+            if user:
+                if user.is_active:
+                    login(request,user)
+                    return redirect("home")
+                else:
+                    return HttpResponse("Your account is disabled")
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        login_form = LoginForm()
+    
+    context['login_form'] = login_form
+    return render(request, "account/login.html", context)

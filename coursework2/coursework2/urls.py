9fb25ed3acb48bc72c4fa447751459c31c5e42e3
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-04 15:37:27 +0800

    update urls for password change

--- coursework2/coursework2/urls.py
+++ coursework2/coursework2/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 from accounts.views import(
     user_register,
@@ -29,7 +30,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_register, name="register"),
     path('login/', user_login, name="login"),
-    path('logout/', user_logout, name="logout")
+    path('logout/', user_logout, name="logout"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_reset/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset/password_change_done.html'), name='password_change_done'),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

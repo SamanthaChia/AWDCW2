a8364607e9469ca43348866fbc5d7e5868790bb0
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-04 23:44:23 +0800

    urls change name

--- coursework2/accounts/urls.py
+++ coursework2/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<user_id>/', views.user_view, name="view")
+    path('<user_id>/', views.user_view, name="user_view")
 ]

9933e91e94b6da409dcb599d9814796080ffc3ce
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-05 21:23:54 +0800

    added url for crop image

--- coursework2/accounts/urls.py
+++ coursework2/accounts/urls.py
@@ -6,4 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path('<user_id>/', views.user_view, name="user_view"),
     path('<user_id>/edit/', views.edit_particulars, name="edit"),
+    path('<user_id>/edit/cropImg', views.crop_image, name="crop_image"),
 ]

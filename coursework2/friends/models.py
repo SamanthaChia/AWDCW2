dbb32b83a3b06c0af542f6977befaf1c17153ccf
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-07 22:12:10 +0800

    create function to call remove_friend

--- coursework2/friends/models.py
+++ coursework2/friends/models.py
@@ -6,17 +6,27 @@ class FriendsList(models.Model):
     # the user id
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user")
     # list of friends, contain pk of friends
-    friendsList = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="friendsList")
+    friends = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="friendsList")
 
     def __str__(self):
         return self.user.username
 
-    def add_friend(self, friend):
-        if friend not in self.friendsList.all():
-            self.friendsList.add(friend)
+    def add_friend(self, account):
+        # check if friend is not in friends list
+        if not account in self.friends.all():
+            self.friends.add(account)
             self.save()
     
-    def remove_friend(self, friend):
-        if friend in self.friendsList.all():
-            self.friendsList.remove(friend)
+    def remove_friend(self, account):
+        # check if friend is in friends list
+        if account in self.friends.all():
+            self.friends.remove(account)
             self.save()
+
+    def unfriend(self, account):
+        user_friend_list = self
+        user_friend_list.remove_friend(account)
+        friends_list = FriendList.objects.get(user=account)
+        friends_list.remove_friend(self.user)
+
+    

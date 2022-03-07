1fd7cf001c454ba25b1b09cf66e7ab9b7ad8740f
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-07 23:31:42 +0800

    Friend Request Admin

--- coursework2/friends/admin.py
+++ coursework2/friends/admin.py
@@ -13,3 +13,14 @@ class FriendsListAdmin(admin.ModelAdmin):
         model = FriendsList
 
 admin.site.register(FriendsList, FriendsListAdmin)
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_filter = ['sender', 'receiver']
+    list_display = ['sender', 'receiver']
+    # sender is a account object, so have to indicate what to search from the object
+    search_fields = ['sender__username', 'sender__email', 'receiver__username', 'receiver__email']
+
+    class Meta:
+        model = FriendRequest
+
+admin.site.register(FriendRequest, FriendRequestAdmin)

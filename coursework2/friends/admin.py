4c4fbc50c50e8c676ca0bb42b20cdf1b2bc0c833
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-07 23:20:40 +0800

    Friend list admin

--- coursework2/friends/admin.py
+++ coursework2/friends/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+class FriendsListAdmin(admin.ModelAdmin):
+    list_filter = ['user']
+    list_display = ['user']
+    search_fields = ['user']
+    readonly_fields = ['user']
+    
+    class Meta:
+        model = FriendsList
+
+admin.site.register(FriendsList, FriendsListAdmin)

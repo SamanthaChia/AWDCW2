c7b9e027e2db86a4969b6eef6182385274da594b
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-07 22:04:25 +0800

    created FriendsList model

--- coursework2/friends/models.py
+++ coursework2/friends/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class FriendsList(models.Model):
+    # the user id
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user")
+    # list of friends, contain pk of friends
+    friendsList = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="friendsList")
+
+    def __str__(self):
+        return self.user.username
+
+

af8dabb04ae4966bdc222577aa16afe59b1f5598
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-08 14:15:28 +0800

    add url in urls.py

--- coursework2/friends/urls.py
+++ coursework2/friends/urls.py
@@ -6,4 +6,5 @@ app_name = "friends"
 urlpatterns = [
     path('friend_request/', views.send_friend_request, name="friend-request"),
     path('friend_request/<user_id>', views.friend_requests, name="friend-requests"),
+    path('accept_friend_request/<friend_request_id>', views.accept_friend_request, name="friend-request-accept"), 
 ]

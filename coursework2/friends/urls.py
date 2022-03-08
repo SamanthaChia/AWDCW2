1348d9771ced84c0533664b577b70e288449103a
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-08 12:05:29 +0800

    change url names

--- coursework2/friends/urls.py
+++ coursework2/friends/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "friends"
 
 urlpatterns = [
-    path('friend_request/', views.send_friend_request_view, name="friend-request"),
-    path('friend_request/<user_id>', views.friend_requests_view, name="friend-requests"),
+    path('send_friend_request/', views.send_friend_request_view, name="friend-request"),
+    path('friend_requests/<user_id>', views.friend_requests_view, name="friend-requests"),
 ]

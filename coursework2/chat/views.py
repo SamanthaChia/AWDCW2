2125112140bf514905d3f8d1814a3e46b52b6101
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-10 20:36:48 +0800

    chat working

--- coursework2/chat/views.py
+++ coursework2/chat/views.py
@@ -5,4 +5,5 @@ def index(request):
     return render(request, 'chat/index.html')
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {'room_name': room_name})
+    username = request.GET.get('username', 'Anonymous')
+    return render(request, 'chat/room.html', {'room_name': room_name, 'username': username})

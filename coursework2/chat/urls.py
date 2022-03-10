63924c709f0023f01221d2ffa9a3181b60e5d54a
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-10 17:31:21 +0800

    changes to url

--- coursework2/chat/urls.py
+++ coursework2/chat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat/index', views.index, name='chat-index'),
-    path('chat/<str:room_name>/', views.room , name='chat-room'),
+    path('chatroom/', views.index, name='chat-index'),
+    path('chatroom/<str:room_name>/', views.room , name='chat-room'),
 ]

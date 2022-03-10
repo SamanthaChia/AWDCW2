e2542696076678e124a39146d82c45f6d87e682f
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-10 20:13:46 +0800

    created asgi.py and websocket able to send messages

--- coursework2/chat/consumers.py
+++ coursework2/chat/consumers.py
@@ -2,44 +2,21 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+    def connect(self):
+        self.accept()
 
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+    def disconnect(self, close_code):
+        pass
     
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        
         message = text_data_json['message']
-        username = text_data_json['username']
-        room = text_data_json['room']
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type' : 'chat_message',
-                'message': message,
-                'username' : username
-            }
-        )
+        self.send(text_data=json.dumps({'message':message}))
 
-    async def chat_message(self, event):
-        message = event['message']
-        username = event['username']
+    # async def chat_message(self, event):
+    #     message = event['message']
 
-        await self.send(text_data=json.dumps({
-            'message':message,
-            'username':username,
-        }))
+    #     await self.send(text_data=json.dumps({
+    #         'message':message,
+    #     }))
--- coursework2/coursework2/routing.py
+++ coursework2/coursework2/routing.py
@@ -1,7 +1,10 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 application = ProtocolTypeRouter({
-    'websocket' : AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+    'websocket' : AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns)
+        ),
 })

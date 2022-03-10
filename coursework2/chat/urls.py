0b52b4c057928c9dc2ef2119667473c3a6695c93
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-10 21:57:05 +0800

    Chat room

--- coursework2/chat/urls.py
+++ coursework2/chat/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "chat"
 
 urlpatterns = [
-    path('chatroom/', views.index, name='chat-index'),
+    path('<user_id>/', views.index, name='chat-index'),
     path('chatroom/<str:room_name>/', views.room , name='chat-room'),
 ]

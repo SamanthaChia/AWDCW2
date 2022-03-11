d3d007853148b5e91f22900fc3f3ac08d9bc567c
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-11 14:23:14 +0800

    added a text area for updating status

--- coursework2/status/forms.py
+++ coursework2/status/forms.py
@@ -2,7 +2,16 @@ from django import forms
 from .models import *
 
 class StatusForm(forms.ModelForm):
-    textUpdate = forms.CharField(required=True)
+    textUpdate = forms.CharField(
+        required=True,
+        widget=forms.widgets.Textarea(
+            attrs={
+                "placeholder": "What's happening today?",
+                "class": "textarea"
+            }
+        ),
+        label="",
+    )
 
     class Meta:
         model = StatusList

715f600429428990ecf8e405278464d7cc178517
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-12 14:40:58 +0800

    changed url pathing for login

--- coursework2/accounts/urls.py
+++ coursework2/accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import *
 
 app_name = "accounts"
 
 urlpatterns = [
-    path('register/', views.user_register, name="register"),
-    path('<user_id>/', views.user_view, name="user_view"),
-    path('<user_id>/edit/', views.edit_particulars, name="edit"),
-    path('<user_id>/edit/cropImg', views.crop_image, name="crop_image"),
+    path('register/', user_register, name="register"),
+    path('login/', user_login, name="login"),
+    path('<user_id>/', user_view, name="user_view"),
+    path('<user_id>/edit/', edit_particulars, name="edit"),
+    path('<user_id>/edit/cropImg', crop_image, name="crop_image"),
 ]

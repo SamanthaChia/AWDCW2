98fa31177fa7fc6ee5741c7446b88802899d998d
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-12 21:30:00 +0800

    changes to urls friend

--- coursework2/friends/urls.py
+++ coursework2/friends/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from . import views
+from .views import *
 
 app_name = "friends"
 
 urlpatterns = [
-    path('friend_request/', views.send_friend_request, name="friend-request"),
-    path('friend_request/<user_id>', views.friend_requests, name="friend-requests"),
-    path('accept_friend_request/<friend_request_id>', views.accept_friend_request, name="friend-request-accept"), 
-    path('remove_friend/', views.remove_friend, name="remove-friend"),
-    path('decline_friend_request/<friend_request_id>', views.decline_friend_request, name="friend-request-decline"), 
-    path('cancel_friend_request/', views.cancel_friend_request, name="friend-request-cancel"),    
-    path('friends_list/<user_id>', views.friends_list, name="friends-list"),    
+    path('friend_request/', send_friend_request, name="friend-request"),
+    path('friend_request/<user_id>', friend_requests, name="friend-requests"),
+    path('accept_friend_request/<friend_request_id>', accept_friend_request, name="friend-request-accept"), 
+    path('remove_friend/', remove_friend, name="remove-friend"),
+    path('decline_friend_request/<friend_request_id>', decline_friend_request, name="friend-request-decline"), 
+    path('cancel_friend_request/', cancel_friend_request, name="friend-request-cancel"),    
+    path('friends_list/<user_id>', friends_list, name="friends-list"),    
 
 ]

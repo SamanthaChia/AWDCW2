27ed31cc7cf4f27df2d529641a9303163c11a14e
Samantha Chia Eileen <samanthacyl1999@gmail.com>
2022-03-12 14:42:58 +0800

    change pathing url for logout

--- coursework2/accounts/urls.py
+++ coursework2/accounts/urls.py
@@ -6,6 +6,7 @@ app_name = "accounts"
 urlpatterns = [
     path('register/', user_register, name="register"),
     path('login/', user_login, name="login"),
+    path('logout/', user_logout, name="logout"),
     path('<user_id>/', user_view, name="user_view"),
     path('<user_id>/edit/', edit_particulars, name="edit"),
     path('<user_id>/edit/cropImg', crop_image, name="crop_image"),
